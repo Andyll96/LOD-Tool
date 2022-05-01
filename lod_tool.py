87c1570ebaf9f2208dbf51e3e62e3c40ff3182b4
allactah-Desktop <andy.llactahuamani@ryerson.ca>
2022-05-01 18:36:52 -0400

    More GUI implementation. populating lists, adding, removing, and refreshing

--- lod_tool.py
+++ lod_tool.py
@@ -11,6 +11,14 @@ class LODUI(QtWidgets.QDialog):
         self.setFixedSize(QtCore.QSize(600,700))
         
         self.library = LODController()
+
+        self.lodList = QtWidgets.QListWidget()
+        self.lodList.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+        self.lodList.setAlternatingRowColors(True)
+        self.lodList.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.cameraComboBox = QtWidgets.QComboBox()
+
+        self.populateCamList()
         self.buildUI()
 
     def buildUI(self):
@@ -21,23 +29,48 @@ class LODUI(QtWidgets.QDialog):
 
         # widgets
         prompt1 = QtWidgets.QLabel('Add and Arrange LOD Models from most to least detailed')
-        lodList = QtWidgets.QListWidget()
         addButton = QtWidgets.QPushButton('Add')
         removeButton = QtWidgets.QPushButton('Remove')
+        refreshButton = QtWidgets.QPushButton('Refresh')
         createButton = QtWidgets.QPushButton('Create')
-        cameraComboBox = QtWidgets.QComboBox()
+
+        # signals
+        addButton.clicked.connect(self.addListItems)
+        removeButton.clicked.connect(self.removeListItems)
+        refreshButton.clicked.connect(self.populateCamList)
+        createButton.clicked.connect(self.createLodCam)
 
         Vlayout.addWidget(prompt1)
-        Vlayout.addWidget(lodList)
+        Vlayout.addWidget(self.lodList)
         Vlayout.addLayout(Hlayout1)
         Vlayout.addLayout(Hlayout2)
 
-        Hlayout1.addWidget(removeButton)
         Hlayout1.addWidget(addButton)
+        Hlayout1.addWidget(removeButton)
         
         Hlayout2.addWidget(createButton)
-        Hlayout2.addWidget(cameraComboBox)
+        Hlayout2.addWidget(refreshButton)
+        Hlayout2.addWidget(self.cameraComboBox)
+
+    def addListItems(self):
+        selections = cmds.ls(selection=True)
+        self.lodList.addItems(selections)
+    
+    def removeListItems(self):
+        listItems = self.lodList.selectedItems()
+        if not listItems: return
+        for item in listItems:
+            self.lodList.takeItem(self.lodList.row(item))
+
+    def createLodCam(self):
+        cmds.camera(name='LodCam')
         
+    def populateCamList(self):
+        self.cameraComboBox.clear()
+        cameralist = cmds.listCameras()
+        print(f'cameraList: {cameralist}')
+        self.cameraComboBox.addItem('Create New Camera')
+        self.cameraComboBox.addItems(cameralist)
 
 def showUI():
     ui = LODUI()
